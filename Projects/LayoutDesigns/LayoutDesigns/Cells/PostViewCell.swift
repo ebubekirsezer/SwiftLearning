eaeda87aac12d1e57eaa473a6338abda06804d55
Ebubekir Sezer <ebubekirssezer@gmail.com>
2021-01-07 18:10:31 +0300

    Post View Cell and Multiple Collection View added

---
+++ Projects/LayoutDesigns/LayoutDesigns/Cells/PostViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostViewCell.swift
+//  LayoutDesigns
+//
+//  Created by Ebubekir Sezer on 7.01.2021.
+//
+
+import UIKit
+
+class PostViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Projects/LayoutDesigns/LayoutDesigns/ViewControllers/TwitterHomeViewController.swift
+++ Projects/LayoutDesigns/LayoutDesigns/ViewControllers/TwitterHomeViewController.swift
@@ -16,6 +16,12 @@ class TwitterHomeViewController: UIViewController {
             storyList.dataSource = self
         }
     }
+    @IBOutlet weak var postList: UICollectionView! {
+        didSet{
+            postList.delegate = self
+            postList.dataSource = self
+        }
+    }
     
     private let people = DataSource.people
     
@@ -25,6 +31,9 @@ class TwitterHomeViewController: UIViewController {
     }
     
     private func registerCells(){
+        let postCell = UINib(nibName: "PostViewCell", bundle: nil)
+        postList.register(postCell, forCellWithReuseIdentifier: "PostViewCell")
+        
         let storyCell = UINib(nibName: "StoryViewCell", bundle: nil)
         storyList.register(storyCell, forCellWithReuseIdentifier: "StoryViewCell")
     }
@@ -32,15 +41,23 @@ class TwitterHomeViewController: UIViewController {
 
 extension TwitterHomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return people.count
+        guard collectionView == postList else {
+            return people.count
+        }
+        return people[section].posts.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StoryViewCell", for: indexPath) as! StoryViewCell
         
-        let person = people[indexPath.row]
-        cell.configureWith(person: person)
-        return cell
+        guard collectionView == postList else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StoryViewCell", for: indexPath) as! StoryViewCell
+            
+            let person = people[indexPath.row]
+            cell.configureWith(person: person)
+            return cell
+        }
+        let postCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostViewCell", for: indexPath) as! PostViewCell
+        return postCell
     }
     
 }
@@ -48,7 +65,10 @@ extension TwitterHomeViewController: UICollectionViewDelegate, UICollectionViewD
 extension TwitterHomeViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 90, height: 90)
+        guard collectionView == postList else {
+            return CGSize(width: 90, height: 90)
+        }
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height / 2)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
